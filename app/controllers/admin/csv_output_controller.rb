47f7cbaf8b7ff1a9f7dc679013565b0a94bb8d11
jeanepaul <jeanepaul@gmail.com>
2013-09-10 12:10:35 +0900

    新サーバーの設定の修正

--- app/controllers/admin/csv_output_controller.rb
+++ app/controllers/admin/csv_output_controller.rb
@@ -215,7 +215,7 @@ class Admin::CsvOutputController < Admin::BaseController
       import_server_name = params[:import_server_id]
       #admin_mark = params[:admin_mark_guid]
       #return flash[:notice] = "標識IDが不正です。" if admin_mark != session[:admin_mark_guid]
-      CsvInput.input(school_id, server_name, project_name, import_server_name)
+      CsvInput.input(school_id, 'doc_togo_dev_a', project_name, import_server_name)
       flash[:notice] = "データを更新しました。"
     else
       #admin_mark = session[:admin_mark_guid]
@@ -241,7 +241,7 @@ class Admin::CsvOutputController < Admin::BaseController
       import_server_name = params[:import_server_id]
       #admin_mark = params[:admin_mark_guid]
       #return flash[:notice] = "標識IDが不正です。" if admin_mark != session[:admin_mark_guid]
-      CsvInput.input(school_id, server_name, project_name, import_server_name)
+      CsvInput.input(school_id, 'doc_togo_dev_a', project_name, import_server_name)
       flash[:notice] = "データを更新しました。"
     else
       #admin_mark = session[:admin_mark_guid]
